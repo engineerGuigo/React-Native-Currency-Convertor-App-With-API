5aa63f42ceebcc797a354c18a88381147e1e8e91
Rodrigo Durante <rodrigodurante@Guilhermes-MacBook-Pro.local>
2020-02-24 16:35:43 -0300

    styled-components test

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Title } from './styles';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Title>
+        Test
+      </Title>
     </View>
   );
 }
